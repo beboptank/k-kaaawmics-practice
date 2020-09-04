16374d83db2dd8b1bedb5eb367e684177550a61a
beboptank <beboptank12@gmail.com>
2020-09-03 23:25:30 -0400

    strapi added as file source

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `K-KAAAWMICS`,
+    description: `Original stories and art. Indie vibe. Freedom. It's K-KAAAWMICS.`,
+    author: `@SethChristmus`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -14,10 +14,15 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-strapi",
       options: {
-        name: `pages`,
-        path: `${__dirname}/src/pages`,
+        apiURL: "http://localhost:1337",
+        contentTypes: [
+          // List of the Content Types you want to be able to request from Gatsby.
+          "article",
+          "user"
+        ],
+        queryLimit: 1000,
       },
     },
     `gatsby-transformer-remark`,
@@ -34,8 +39,6 @@ module.exports = {
         display: `minimal-ui`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    'gastby-plugin-offline',
   ],
 }
---
+++ src/pages/2020-07-18-sapients-preview/index.md
@@ -0,0 +1,8 @@
+---
+path: /sapients-preview
+date: 2020-07-18
+title: Sapients Preview
+author: VVQ
+description: Sapients Preview
+featuredImage: 
+---
